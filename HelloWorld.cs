2e3c3ef562fe6e36cfd801fff52c0c3392235d7f
Abatmon <152018579+Abatmon@users.noreply.github.com>
2023-11-25 17:44:04 +0200

    Update and rename README.md to HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+# Scripting
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this is a message
+        print("Hello,World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
